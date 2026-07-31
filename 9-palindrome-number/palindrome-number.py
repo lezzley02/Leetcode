7942629abaf43bfc1c0e9294e7da1585f96f56d8
Leslie Kudakwashe Chibumba <lesliechibumba05@gmail.com>
2026-07-31 14:05:57 +0530

    Time: 7 ms (78.01%) | Memory: 12.4 MB (55.11%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,18 +1,10 @@
 class Solution(object):
     def isPalindrome(self, x):
-         if x < 0:
-               return False
+         r = str(x)[::-1]
 
-         original=x
-         reverse=0
-
-         while x>0:
-              digit=x % 10
-              reverse=reverse *10 + digit
-              x=x//10
-         if original==reverse :
-              return True
-         else :
+         if r != str(x):
              return False
+         else :
+             return True
            
         
